26d5adc9ea13fb0a27a96c158d4551d0b403792c
panicbit <panicbit.dev@gmail.com>
2018-07-31 22:53:56 +0200

    Rename project to plz

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ struct Opt {
 
 fn main() {
     match do_main() {
-        Ok(pls) => pls,
+        Ok(plz) => plz,
         Err(e) => {
             println!("{}", e);
             for cause in e.causes().skip(1) {
@@ -36,24 +36,24 @@ fn main() {
 
 fn do_main() -> Result<()> {
     let opt = Opt::from_args();
-    let (path, pls) = load_pls_toml()?;
+    let (path, plz) = load_plz_toml()?;
 
     env::set_current_dir(path)?;
 
     for task in opt.tasks {
-        pls.run_task(&task)?;
+        plz.run_task(&task)?;
     }
 
     Ok(())
 }
 
-fn load_pls_toml() -> Result<(PathBuf, Pls)> {
+fn load_plz_toml() -> Result<(PathBuf, Plz)> {
     use std::fs;
 
     let mut path = env::current_dir()?;
 
     loop {
-        path.push("pls.toml");
+        path.push("plz.toml");
 
         if path.is_file() {
             break;
@@ -62,25 +62,25 @@ fn load_pls_toml() -> Result<(PathBuf, Pls)> {
         path.pop();
         
         if !path.pop() {
-            bail!("No 'pls.toml' found in current or any parent folder");
+            bail!("No 'plz.toml' found in current or any parent folder");
         }
     }
 
     let file = fs::read_to_string(&path)?;
-    let pls = toml::from_str::<Pls>(&file)?;
+    let plz = toml::from_str::<Plz>(&file)?;
 
     path.pop();
 
-    Ok((path, pls))
+    Ok((path, plz))
 }
 
 #[derive(Deserialize, Default)]
 #[serde(default, deny_unknown_fields)]
-struct Pls {
+struct Plz {
     tasks: HashMap<String, Arc<Task>>,
 }
 
-impl Pls {
+impl Plz {
     fn run_task(&self, task: &str) -> Result<()> {
         let task = self.tasks.get(task)
             .cloned()
